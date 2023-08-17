3ba68020eb5dbbd60c9fb39288a86a589d5ca04b
abdulazizxoldorov <abdulazizxoldorov05@gmail.com>
2023-08-17 17:16:26 +0500

    kjkn

--- src/components/addHodimBody/addHodimBody.jsx
+++ src/components/addHodimBody/addHodimBody.jsx
@@ -174,93 +174,12 @@ const AddHodimBody = () => {
           <Box display={"flex"} flexDirection={"column"}>
             <FormLabel>Qiziqishlari</FormLabel>
 
-<<<<<<< HEAD
             <Select>
               {interest.map((item) => (
                 <option key={item.id}> {item.name} </option>
               ))}
             </Select>
           </Box>
-=======
-                {/* ma'lumot boshlandi */}
-                <Box display={'flex'} flexDirection={{base: 'column' , xl: 'row'}} gap={'30px'}>
-                    <FormControl isRequired>
-                        <FormLabel>Ma'lumoti</FormLabel>
-                        <Select options={options} defaultInputValue={allVal} placeholder={"Ma'lumotni tanlang"} onChange={setAllVal}  noOptionsMessage={() => "Bunday Ma'lumot Mavjud Emas"} isSearchable />
-                    </FormControl>
-
-                    <FormControl isRequired>
-                        <FormLabel>Telefon raqami (Asosiy)</FormLabel>
-                        <Input placeholder='+998' />
-                    </FormControl>
-                </Box>
-                {/* ma'lumot tugadi */}
-
-
-                {/* qo'shimcha boshlandi */}
-                <Box display={'flex'} flexDirection={{base: 'column' , xl: 'row'}} gap={'30px'}>
-                    <FormControl isRequired>
-                        <FormLabel>Telefon raqami (Qo’shimcha)</FormLabel>
-                        <Input  placeholder='Tel...' />
-                    </FormControl>
-
-                    <FormControl isRequired>
-                        <FormLabel>Telegram (username)</FormLabel>
-                        <Input placeholder='Telegram...' />
-                    </FormControl>
-                </Box>
-                {/* qo'shimcha tugadi */}
-
-                <Box display={'flex'} flexDirection={{base: 'column' , xl: 'row'}} gap={'30px'}>
-                    <FormControl isRequired>
-                        <FormLabel>Instagram (username)</FormLabel>
-                        <Input width={{base: '100%' ,  xl: '48.5%', '2xl': '50%'}} placeholder='Instagram...' />
-                    </FormControl>
-                </Box>
-
-                <Box display={'flex'} flexDirection={ 'column'}>
-                    <FormLabel>Qiziqishlari</FormLabel>
-                    
-                    <Select styles={{
-                        control: (baseStyle) => ({
-                            ...baseStyle,
-                            paddingBottom: '70px'
-                        }),
-                        dropdownIndicator: () => ({
-                            display: 'none'
-                        }),
-                        multiValueRemove: (baseStyle , state) => ({
-                            ...baseStyle,
-                            color: state.isFocused ? 'red' : 'white',
-                            backgroundColor: state.isFocused ? "black" : '#5D5FEF'
-                        })
-                    }}  isMulti  isClearable options={Interest} defaultInputValue={inter} placeholder={"Qiziqishni tanlang"} onChange={setInter}  noOptionsMessage={() => "Bunday Ma'lumot Mavjud Emas"} isSearchable />
-                </Box>
-
-            </Box>
-
-            <Box mt={'20px'} >
-                <Box  width={{base: '100%' , '2xl': '400px'}} overflow={'auto'} display={'flex'} alignItems={'center'} justifyContent={'center'} rounded={'10px'} height={'300px'} bg={'#F0F0FF'}>
-                    <form action="" >
-                        <input  className='input-field'  hidden type="file" accept='image/*' onChange={({target: {files}}) => {
-                        if(files) {
-                            setImage(URL.createObjectURL(files[0]))
-                        }
-                        }}/>
-                        {image ? 
-                        <img src={image} width={'380px'}  alt={fileName} />
-                        : (
-                        <Heading fontSize={'32px'}>Hodim rasmi</Heading>
-                        )
-                    }
-                    </form>
-                </Box>
-                <Box >
-                    <Button onClick={() => document.querySelector('.input-field').click()} mt={'20px'} bg={'#10B981'} color={'#fff'} _hover={{bg: ''}} _active={{bg: ''}} width={'100%'}>Rasm tanlash</Button>
-                </Box>
-            </Box>
-
->>>>>>> 84022d4d65cada37175521788de2a4a15fc5f5c5
         </Box>
 
         <Box mt={"20px"}>
